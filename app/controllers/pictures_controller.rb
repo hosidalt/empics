cc46ab5305e8605241cfb7e183c5ce5029982e41
hosidalt <itie0112@yahoo.co.jp>
2019-02-20 18:19:26 +0900

    .chat-group-user.clearfix

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,8 +1,18 @@
 class PicturesController < ApplicationController
-  def index
-  end
+  # def index
+  #   @pictures = Picture.all
+  # end
 
-  def create
-  end
+  # def new
+  # end
 
+#   def create
+#     Picture.create(image: picture_params[:image],user_id: current_user.id)
+#   end
+
+
+# private
+#   def picture_prams
+#     params.permit(:image)
+#   end
 end
